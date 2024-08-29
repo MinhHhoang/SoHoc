50db79329c2376727c15bed23c2cea11fa94a8f0
dev-hoale <leconghoa11a1@gmail.com>
2024-08-29 08:17:26 +0700

    update

--- src/pages/ManagerData/index.jsx
+++ src/pages/ManagerData/index.jsx
@@ -26,6 +26,7 @@ import {
   getStatistic,
   resetData,
 } from "store/ManagerData/action";
+import listRecommend from "./data.json";
 
 function ManagerData(props) {
   const {
@@ -105,231 +106,6 @@ function ManagerData(props) {
   };
 
   const toggleVisible = () => setVisible(!visible);
-
-  const listRecommend = [
-    {
-      name: "Chẵn lẻ",
-      value:
-        "01, 03, 05, 07, 09, 21, 23, 25, 27, 29, 41, 43, 45, 47, 49, 61, 63, 65, 67, 69, 81, 83, 85, 87, 89",
-      money: "",
-    },
-    {
-      name: "Lẻ chẵn",
-      value:
-        "10, 12, 14, 16, 18, 30, 32, 34, 36, 38, 50, 52, 54, 56, 58, 70, 72, 74, 76, 78, 90, 92, 94, 96, 98",
-      money: "",
-    },
-    {
-      name: "Chẵn chẵn",
-      value:
-        "00, 02, 04, 06, 08, 20, 22, 24, 26, 28, 40, 42, 44, 46, 48, 60, 62, 64, 66, 68, 80, 82, 84, 86, 88",
-      money: "",
-    },
-    {
-      name: "Lẻ lẻ",
-      value:
-        "11, 13, 15, 17, 19, 31, 33, 35, 37, 39, 51, 53, 55, 57, 59, 71, 73, 75, 77, 79, 91, 93, 95, 97, 99",
-      money: "",
-    },
-    {
-      name: "Tổng trên 10",
-      value:
-        "19, 28 - 29, 37 - 38 - 39, 46 - 47 - 48 - 49, 55 - 56 - 57 - 58 - 59, 64 - 65 - 66 - 67 - 68 - 69, 73 - 74 - 75 - 76 - 77 - 78 - 79, 82 - 83 - 84 - 85 - 86 - 87 - 88 - 89, 91 - 92 - 93 - 94 - 95 - 96 - 97 - 98 - 99",
-      money: "",
-    },
-    {
-      name: "Tổng dưới 10",
-      value:
-        "00 - 01 - 02 - 03 - 04 - 05 - 06 - 07 - 08 - 09, 10 - 11 - 12 - 13 - 14 - 15 - 16 - 17 - 18, 20 - 21 - 22 - 23 - 24 - 25 - 26 - 27, 30 - 31 - 32 - 33 - 34 - 35 - 36, 40 - 41 - 42 - 43 - 44 - 45, 50 - 51 - 52 - 53 - 54, 60 - 61 - 62 - 63, 70 - 71 - 72, 80 - 81, 90",
-      money: "",
-    },
-    {
-      name: "Tổng 10",
-      value: "19 - 91 - 28 - 82 - 37 - 73 - 46 - 64 - 55",
-      money: "",
-    },
-    {
-      name: "To nhỏ",
-      value:
-        "90, 91, 92, 93, 94, 80, 81, 82, 83, 84, 70, 71, 72, 73, 74, 60, 61, 62, 63, 64, 50, 51, 52, 53, 54",
-      money: "",
-    },
-    {
-      name: "Nhỏ to",
-      value:
-        "05, 06, 07, 08, 09, 15, 16, 17, 18, 19, 25, 26, 27, 28, 29, 35, 36, 37, 38, 39, 45, 46, 47, 48, 49",
-      money: "",
-    },
-    {
-      name: "To to",
-      value:
-        "55, 66, 77, 88, 99, 56, 65, 57, 75, 58, 85, 59, 95, 67, 76, 68, 86, 69, 96, 78, 87, 79, 97, 89, 98",
-      money: "",
-    },
-    {
-      name: "Nhỏ nhỏ",
-      value:
-        "00, 11, 22, 33, 44, 01, 10, 02, 20, 03, 30, 04, 40, 12, 21, 13, 31, 14, 41, 23, 32, 24, 42, 34, 43",
-      money: "",
-    },
-    {
-      name: "Tổng 0",
-      value: "00 - 19 - 28 - 37 - 46 - 55 - 64 - 73 - 82 - 91",
-      money: "",
-    },
-    {
-      name: "Tổng 1",
-      value: "01 - 10 - 29 - 38 - 47 - 56 - 65 - 74 - 83 - 92 ",
-      money: "",
-    },
-    {
-      name: "Tổng 2",
-      value: "02 - 11 - 20 - 39 - 48 - 57 - 66 - 75 - 84 - 93",
-      money: "",
-    },
-    {
-      name: "Tổng 3",
-      value: "03 - 12 - 21 - 30 - 49 - 58 - 67 - 76 - 85 - 94 ",
-      money: "",
-    },
-    {
-      name: "Tổng 4",
-      value: "04 - 13 - 22 - 31 - 40 - 59 - 68 - 77 - 86 - 95",
-      money: "",
-    },
-    {
-      name: "Tổng 5",
-      value: "05 - 14 - 23 - 32 - 41 - 50 - 69 - 78 - 87 - 96",
-      money: "",
-    },
-    {
-      name: "Tổng 6",
-      value: "06 - 15 - 24 - 33 - 42 - 51 - 60 - 79 - 88 - 97",
-      money: "",
-    },
-    {
-      name: "Tổng 7",
-      value: "07 - 16 - 25 - 34 - 43 - 52 - 61 - 70 - 89 - 98",
-      money: "",
-    },
-    {
-      name: "Tổng 8",
-      value: "08 - 17 - 26 - 35 - 44 - 53 - 62 - 71 - 80 - 99",
-      money: "",
-    },
-    {
-      name: "Tổng 9",
-      value: "09 - 18 - 27 - 36 - 45 - 54 - 63 - 72 - 81 - 90",
-      money: "",
-    },
-    {
-      name: "Chập",
-      value:
-        "01,10,12, 21, 23, 32, 34, 43, 54, 45, 65, 56, 67, 76, 78, 87, 89, 98, 09, 90",
-      money: "",
-    },
-
-    {
-      name: "Đầu 0",
-      value: "00, 01, 02, 03, 04, 05, 06, 07, 08, 09",
-      money: "",
-    },
-    {
-      name: "Đầu 1",
-      value: "10, 11, 12, 13, 14, 15, 16, 17, 18, 19",
-      money: "",
-    },
-    {
-      name: "Đầu 2",
-      value: "20, 21, 22, 23, 24, 25, 26, 27, 28, 29",
-      money: "",
-    },
-    {
-      name: "Đầu 3",
-      value: "30, 31, 32, 33, 34, 35, 36, 37, 38, 39",
-      money: "",
-    },
-    {
-      name: "Đầu 4",
-      value: "40, 41, 42, 43, 44, 45, 46, 47, 48, 49",
-      money: "",
-    },
-    {
-      name: "Đầu 5",
-      value: "50, 51, 52, 53, 54, 55, 56, 57, 58, 59",
-      money: "",
-    },
-    {
-      name: "Đầu 6",
-      value: "60, 61, 62, 63, 64, 65, 66, 67, 68, 69",
-      money: "",
-    },
-    {
-      name: "Đầu 7",
-      value: "70, 71, 72, 73, 74, 75, 76, 77, 78, 79",
-      money: "",
-    },
-    {
-      name: "Đầu 8",
-      value: "80, 81, 82, 83, 84, 85, 86, 87, 88, 89",
-      money: "",
-    },
-    {
-      name: "Đầu 9",
-      value: "90, 91, 92, 93, 94, 95, 96, 97, 98, 99",
-      money: "",
-    },
-    {
-      name: "Đuôi 0",
-      value: "00, 10, 20, 30, 40, 50, 60, 70, 80, 90",
-      money: "",
-    },
-    {
-      name: "Đuôi 1",
-      value: "01, 11, 21, 31, 41, 51, 61, 71, 81, 91",
-      money: "",
-    },
-    {
-      name: "Đuôi 2",
-      value: "02, 12, 22, 32, 42, 52, 62, 72, 82, 92",
-      money: "",
-    },
-    {
-      name: "Đuôi 3",
-      value: "03, 13, 23, 33, 43, 53, 63, 73, 83, 93",
-      money: "",
-    },
-    {
-      name: "Đuôi 4",
-      value: "04, 14, 24, 34, 44, 54, 64, 74, 84, 94",
-      money: "",
-    },
-    {
-      name: "Đuôi 5",
-      value: "05, 15, 25, 35, 45, 55, 65, 75, 85, 95",
-      money: "",
-    },
-    {
-      name: "Đuôi 6",
-      value: "06, 16, 26, 36, 46, 56, 66, 76, 86, 96",
-      money: "",
-    },
-    {
-      name: "Đuôi 7",
-      value: "07, 17, 27, 37, 47, 57, 67, 77, 87, 97",
-      money: "",
-    },
-    {
-      name: "Đuôi 8",
-      value: "08, 18, 28, 38, 48, 58, 68, 78, 88, 98",
-      money: "",
-    },
-    {
-      name: "Đuôi 9",
-      value: "09, 19, 29, 39, 49, 59, 69, 79, 89, 99",
-      money: "",
-    },
-  ];
 
   return (
     <div>
@@ -416,10 +192,10 @@ function ManagerData(props) {
                   aria-label="Giá tiền"
                   placeholder="Giá tiền"
                   name="money"
-                  onValueChange={({ value }) =>
+                  onValueChange={({ floatValue }) =>
                     setData((prev) => ({
                       ...prev,
-                      money: value,
+                      money: floatValue,
                     }))
                   }
                   allowNegative={false} // Không cho phép số âm
@@ -550,6 +326,9 @@ function ManagerData(props) {
                         #
                       </th>
                       <th scope="col" className="align-middle">
+                        TÊN
+                      </th>
+                      <th scope="col" className="align-middle">
                         DÀN ĐỀ
                       </th>
                       <th
@@ -591,6 +370,9 @@ function ManagerData(props) {
                           {index + 1}
                         </th>
                         <td className="align-middle">
+                          <b>{item.name || "_"}</b>
+                        </td>
+                        <td className="align-middle">
                           <b>{item.value}</b>
                         </td>
                         <td className="align-middle">
@@ -602,6 +384,7 @@ function ManagerData(props) {
                               setData({
                                 value: item.value,
                                 money: item.money,
+                                name: item?.name,
                                 id: item.id,
                               })
                             }
