154c7663b9f13d620710eacb1b8eb8ef7d8efb58
MinhHhoang <70305905+MinhHhoang@users.noreply.github.com>
2024-09-05 14:35:09 +0700

    UPDATE

--- src/pages/ManagerData/index.jsx
+++ src/pages/ManagerData/index.jsx
@@ -103,7 +103,7 @@ export default function ManagerData(props) {
 
   useEffect(() => {
     if (
-      actionSuccess || copyStatus.isSuccess  ||
+      actionSuccess || 
       (plusMoneyStatus.isSuccess && plusMoneyStatus.type === "single")
     ) {
       onCloseTooltip();
@@ -112,7 +112,7 @@ export default function ManagerData(props) {
       onGetList();
       setVisible(false);
     }
-  }, [actionSuccess, plusMoneyStatus.isSuccess, copyStatus.isSuccess]);
+  }, [actionSuccess, plusMoneyStatus.isSuccess, copyStatus]);
 
   useEffect(() => {
     if (settingStatus.isSuccess) {
@@ -122,7 +122,7 @@ export default function ManagerData(props) {
 
   useEffect(() => {
     if (
-      updateMoneyStatus.isSuccess || copyStatus.isSuccess || 
+      updateMoneyStatus.isSuccess 
       (plusMoneyStatus.isSuccess && plusMoneyStatus.type === "multi")
     ) {
       onGetStatistic();
@@ -130,7 +130,7 @@ export default function ManagerData(props) {
       setSelected([]);
       onGetList();
     }
-  }, [updateMoneyStatus.isSuccess, plusMoneyStatus.isSuccess, copyStatus.isSuccess]);
+  }, [updateMoneyStatus.isSuccess, plusMoneyStatus.isSuccess, copyStatus]);
 
   useEffect(() => {
     if (updateAdvanceStatus.isSuccess) {
